c2cc88e0875667f5a96ec637ab282b99ce1a00ac
nashemistoart@gmail.com <nashemistoart@gmail.com>
2021-07-21 20:38:14 +0300

    first commit

---
+++ WebAppSite/WebAppSite/Controllers/AnimalController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppSite.Models;
+
+namespace WebAppSite.Controllers
+{
+    public class AnimalController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<AnimalViewModel> model = new List<AnimalViewModel>();
+            model.Add(new AnimalViewModel
+            {
+                Id=1,
+                Birthday=DateTime.Now,
+                Name="Енот-паласкун",
+                Image="1.jpg"
+            });
+            model.Add(new AnimalViewModel
+            {
+                Id = 2,
+                Birthday = DateTime.Now.AddDays(-5),
+                Name = "Кот-рыжий",
+                Image = "2.jpg"
+            });
+            return View(model);
+        }
+    }
+}
---
+++ WebAppSite/WebAppSite/Models/AnimalViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppSite.Models
+{
+    public class AnimalViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Image { get; set; }
+    }
+}
